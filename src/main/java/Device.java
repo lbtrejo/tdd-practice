49bd3b49fdcb17c701cae4c57daedd8dfbba1460
Levi Trejo <levi.b.trejo@gmail.com>
2020-10-21 09:31:32 -0500

    feat: test conditions added for warmup

---
+++ src/main/java/Device.java
@@ -0,0 +1,8 @@
+public class Device {
+
+    private String name;
+
+    public Device(String name){
+        this.name = name;
+    }
+}
--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,9 +1,35 @@
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class MyFirstTest {
 
-    
+    private List<String> names;
+
+    @Before
+    public void setUp(){
+        this.names = new ArrayList<>();
+        this.names.add("Fer");
+    }
+
+    // We can verify that the names list is not longer null because it was initialized in the setUp() method with the following test:
+    @Test
+    public void testIfNamesIsInitialized(){
+        assertNotNull(names);
+    }
+
+    @Test
+    public void testIfANameCanBeAdded(){
+        assertEquals(1, this.names.size());
+        this.names.add("Zach");
+        assertEquals(2, this.names.size());
+        assertSame("Fer", this.names.get(0));
+        assertSame("Zach", this.names.get(1));
+    }
 
     @Test
     public void testIfCompanyNameIsEquals() {
@@ -29,5 +55,59 @@ public class MyFirstTest {
         char[] actual = "Junit".toCharArray();
 
         assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testIfObjectsAreDifferent() {
+        Object dog = new Object();
+        Object sheep = new Object();
+        Object clonedSheep = sheep;
+
+        assertNotSame(sheep, dog);
+        assertSame(sheep, clonedSheep);
+    }
+
+    @Test
+    public void testIfGreaterOrLesserThanWorks() {
+
+        boolean learningTDD = true;
+
+        assertTrue(learningTDD);
+        assertTrue("5 is greater than 4", 5 > 4);
+        assertFalse("5 is not greater than 6", 5 > 6);
+    }
+
+    @Test
+    public void testIfInstanceIsNull() {
+        Device phone = new Device("iPhone");
+        Device laptop = null;
+        assertNull(null);
+        assertNotNull(phone);
+    }
+
+    @Test
+    public void testIfArraysAreSame(){
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+
+        assertNotSame(languages, moreLanguages);
+    }
+
+    @Test
+    public void testArrayEquality(){
+        int[] numbers = {1, 2, 3};
+        int[] moreNumbers = new int[3];
+        moreNumbers[0] = 1;
+        moreNumbers[1] = 2;
+        moreNumbers[2] = 3;
+
+        assertArrayEquals(numbers, moreNumbers);
+    }
+
+    @Test
+    public void testStringContains(){
+        String language = "PHP";
+        assertTrue(language.contains("H"));
+        assertFalse(language.contains("J"));
     }
 }
