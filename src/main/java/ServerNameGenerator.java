dc3de8437109c912e04e10e2f8104629b5eda6d9
Levi Trejo <levi.b.trejo@gmail.com>
2020-10-23 09:10:00 -0500

    feat: tests and code for nounArray

---
+++ src/main/java/ServerNameGenerator.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ServerNameGenerator {
+
+    private static final String[] nounArray = {"fox", "giraffe", "ferret", "dog", "cat", "fly", "reindeer", "camel", "coyote", "hamster"};
+    private static final String[] adjectiveArray = {"zippy", "vexed", "uptight", "timely", "selfish", "perplexed", "nervous", "joyous", "irritable", "graceful"};
+
+    public static String[] getNounArray() {
+        return nounArray;
+    }
+}
--- src/test/java/ServerNameGenTest.java
+++ src/test/java/ServerNameGenTest.java
@@ -3,4 +3,10 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 
 public class ServerNameGenTest {
+
+    @Test
+    public void testNounArrayProperties(){
+        assertEquals("Ensure the noun array has 10 items", 10, ServerNameGenerator.getNounArray().length, 0);
+        assertEquals("Ensure the first item in the noun array is 'fox'", "fox", ServerNameGenerator.getNounArray()[0]);
+    }
 }
